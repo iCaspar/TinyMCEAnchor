e956e4b3134ded0f11ac05bff4cebc33a5499407
Caspar Green <caspar@icaspar.net>
2015-06-11 00:52:19 -0400

    Update tinymceanchor.php with filter for loading anchor plugin

--- tinymceanchor.php
+++ tinymceanchor.php
@@ -16,18 +16,37 @@ Text Domain: cjg_tinymceanchor
 Domain Path: /languages
 */
 
-add_action("admin_init","tinymce_mods_buttons_setup");
+add_action( 'admin_init', 'tinymce_mods_buttons_setup' );
 
+/**
+ * Checks user permissions and adds editor filters
+ * @return null
+ */
 function tinymce_mods_buttons_setup() {
-	//only if editing permissions do we bother
-	if (!current_user_can('edit_posts') && !current_user_can('edit_pages')) return;
+	// if user can't edit don't bother
+	if ( ! current_user_can( 'edit_posts' ) && ! current_user_can( 'edit_pages' ) ) return;
 
-	if ( get_user_option('rich_editing') == 'true') {
-		add_filter('mce_buttons', 'tinymce_mods_add_buttons');
-	}
+	add_filter( 'mce_buttons', 'tinymce_mods_add_buttons');
+	add_filter( 'mce_external_plugins', 'tinymce_mods_register_js' );
 }
 
-function tinymce_mods_add_buttons($buttons) {
-   array_push($buttons, "anchor");
+/**
+ * Add the anchor (bookmark) button to the top row of the visual editor
+ * @param  array $buttons buttons to show on editor row
+ * @return array          buttons including anchor button
+ */
+function tinymce_mods_add_buttons( $buttons ) {
+   $insert = array( 'anchor' );
+   array_splice( $buttons, 12, 0, $insert );
    return $buttons;
+}
+
+/**
+ * Add the TinyMCE plugin script for the anchor button
+ * @param  array $plugin_array plugins registered with TinyMCE
+ * @return array               modified array (with anchor plugin registered)
+ */
+function tinymce_mods_register_js( $plugin_array ) {
+	$plugin_array['anchor'] = plugins_url( '/anchor.min.js', __FILE__ );
+	return $plugin_array;
 }
